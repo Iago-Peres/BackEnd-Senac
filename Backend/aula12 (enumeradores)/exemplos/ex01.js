404b5602051464eecc460cbfb576e78748b3c014
Iago-Peres <138594888+Iago-Peres@users.noreply.github.com>
2024-07-16 11:55:03 -0300

    Add files via upload

--- Backend/aula12 (enumeradores)/exemplos/ex01.js
+++ Backend/aula12 (enumeradores)/exemplos/ex01.js
@@ -8,4 +8,23 @@ var DiasDaSemana;
     DiasDaSemana[DiasDaSemana["Sabado"] = 5] = "Sabado";
     DiasDaSemana[DiasDaSemana["Domingo"] = 6] = "Domingo";
 })(DiasDaSemana || (DiasDaSemana = {}));
-console.log(DiasDaSemana); //Saida: 0
+//console.log(DiasDaSemana); //Saida: 0
+function diaMensagem(dia) {
+    switch (dia) {
+        case DiasDaSemana.Segunda:
+            return "comeca a semana";
+        case DiasDaSemana.Terca:
+            return "e terca feira";
+        case DiasDaSemana.Quarta:
+            return "e quarta feira";
+        case DiasDaSemana.Quinta:
+            return "e quinta feira";
+        case DiasDaSemana.Sexta:
+            return "e sexta feira";
+        case DiasDaSemana.Sabado:
+            return "e sabado";
+        case DiasDaSemana.Domingo:
+            return "e domingo";
+    }
+}
+console.log(diaMensagem(2));
--- Backend/aula12 (enumeradores)/exemplos/ex01.ts
+++ Backend/aula12 (enumeradores)/exemplos/ex01.ts
@@ -8,4 +8,25 @@ enum DiasDaSemana {
     Domingo,
 }
 
-console.log(DiasDaSemana); //Saida: 0
+//console.log(DiasDaSemana); //Saida: 0
+
+function diaMensagem(dia: DiasDaSemana): string {
+    switch (dia) {
+        case DiasDaSemana.Segunda:
+            return "comeca a semana";
+        case DiasDaSemana.Terca:
+            return "e terca feira";
+        case DiasDaSemana.Quarta:
+            return "e quarta feira";
+        case DiasDaSemana.Quinta:
+            return "e quinta feira";
+        case DiasDaSemana.Sexta:
+            return "e sexta feira";
+        case DiasDaSemana.Sabado:
+            return "e sabado";
+        case DiasDaSemana.Domingo:
+            return "e domingo";
+    }
+}
+
+console.log(diaMensagem(2));
