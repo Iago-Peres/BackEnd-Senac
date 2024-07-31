43238169fbe2a9d1e7b5b3810ade8a59d5f98f9e
Iago-Peres <138594888+Iago-Peres@users.noreply.github.com>
2024-07-31 11:39:41 -0300

    Add files via upload

---
+++ Backend/aula21/exemplos/procedures.sql
@@ -0,0 +1,13 @@
+DELIMITER //
+
+CREATE PROCEDURE  nome_procedure(
+    IN nome_parametro1 TIPO,
+    IN nome_parametro2 TIPO
+)
+BEGIN
+    --COMANDOS sql normais--
+
+    -- WHERE local_na_tabela == nome_parametro* --
+END
+
+// DELIMITER;
